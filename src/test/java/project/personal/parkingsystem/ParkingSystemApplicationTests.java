114f8e9c169a97eba84585a145d76008ddf47314
Antmendes <97694520+Antmendes@users.noreply.github.com>
2022-08-19 09:10:31 -0300

    first commit

---
+++ src/test/java/project/personal/parkingsystem/ParkingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package project.personal.parkingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
